0e5513dcf4ea75d3fb18b8cd8c99b389abd60638
Ph0non <mklemann@arcor.de>
2013-12-02 23:57:14 +0100

    small updates to prepare a database in the future

--- dev/Jspidy_sqlite.jl
+++ dev/Jspidy_sqlite.jl
@@ -1,24 +1,23 @@
 using Jspidy
 using SQLite
 using DataFrames
-using Datetime
+using Date
 
-#db_name = "history.sqlite"
-db_name = "./dev/test.sqlite";
+db_name = "history.sqlite";
+SQLite.connect(pwd() * "/dev/" * db_name);
 
-connect(db_name);
-
+# save all constant data for savin disk space 
 raw = Jspidy.get();
-# createtable(raw[:,1:7], name="const_data", infer=true);
 # query("create table data_link (t1key INTEGER PRIMARY KEY, input_date)")
 
-
+# drop table of const data and create new for additional items
 if size(raw,1) > query("select count(*) from const_data")[1,1]
 	droptable("const_data");
 	createtable(raw[:,1:7], name="const_data", infer=true);
 end
 
-dt = datetime("yyyy-MM-dd HH", string(now()));
+dt = string(today(), " ", hour(now()));
+#dt = datetime("yyyy-MM-dd HH", string(now()));
 var_data = raw[:, [1; 8:14]];
 createtable(var_data, name="'$(string(dt))'", infer=true);
 
